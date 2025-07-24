5176042ebbae99d28c87b977b720299f0178b147
supervedovatto <thiagovedovatto@gmail.com.br>
2025-07-24 09:46:39 -0300

    Remoção do SpaceVim e configurações do LazyVim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  -- add gruvbox
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "latte",
+    },
+  },
+}
